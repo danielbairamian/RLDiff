6e843afd07eb157687fc73d6df6b6dac702a4a84
Daniel Bairamian <daniel.bairamian@mail.mcgill.ca>
2026-02-16 15:57:05 -0500

    Nth order just in case but not main focus + not that fast

---
+++ MILA_jobs/train_PPO_nth.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --partition=unkillable                                                                # Ask for unkillable job
+#SBATCH --cpus-per-task=4                                                               # Ask for 4 CPUs
+#SBATCH --gres=gpu:l40s:1                                                                           # Ask for 1 GPU (any GPU)
+#SBATCH --mem=32G                                                                        # Ask for 48 GB of RAM
+#SBATCH --time=48:00:00                                                                  # The job will run for 120 hours
+#SBATCH -o /network/scratch/d/daniel.bairamian/RLDiff_data/SLURM_DUMP/slurm-%j.out  # Write the log on scratch
+
+# 1. Load the required modules
+module --quiet load anaconda/3
+
+# 2. Load your environment
+conda activate RLDiff
+
+# 3. Launch your job
+
+python /home/mila/d/daniel.bairamian/RLDiff/train_PPO_nthorder.py --dataset CIFAR10  --batch_size 64 --sample_multiplier 4  --budget 10 --target_steps 4096 --minibatch_size 256  --num_epochs 1000  --fused_dims 64  --time_encoder_dims 32 64  --projection_dims 256 128 64  --latent_dim 512  --latent_channels 32 64 128 256  --base_dataset_path /network/scratch/d/daniel.bairamian/RLDiff_data/datasets/  --base_logs_path /network/scratch/d/daniel.bairamian/RLDiff_data/logs/PPO/IADB/  --base_path_diffusion /network/scratch/d/daniel.bairamian/RLDiff_data/logs/diffusion/IADB/ 
+python /home/mila/d/daniel.bairamian/RLDiff/train_PPO_nthorder.py --dataset MNIST    --batch_size 64 --sample_multiplier 4  --budget 10 --target_steps 4096 --minibatch_size 256  --num_epochs 1000  --fused_dims 64  --time_encoder_dims 32 64  --projection_dims 256 128 64  --latent_dim 512  --latent_channels 32 64 128 256  --base_dataset_path /network/scratch/d/daniel.bairamian/RLDiff_data/datasets/  --base_logs_path /network/scratch/d/daniel.bairamian/RLDiff_data/logs/PPO/IADB/  --base_path_diffusion /network/scratch/d/daniel.bairamian/RLDiff_data/logs/diffusion/IADB/  
+python /home/mila/d/daniel.bairamian/RLDiff/train_PPO_nthorder.py --dataset CelebAHQ --batch_size 64 --sample_multiplier 2  --budget 10 --target_steps 4096 --minibatch_size 256  --num_epochs 1000  --fused_dims 64  --time_encoder_dims 32 64  --projection_dims 256 128 64  --latent_dim 512  --latent_channels 32 64 128 256  --base_dataset_path /network/scratch/d/daniel.bairamian/RLDiff_data/datasets/  --base_logs_path /network/scratch/d/daniel.bairamian/RLDiff_data/logs/PPO/IADB/  --base_path_diffusion /network/scratch/d/daniel.bairamian/RLDiff_data/logs/diffusion/IADB/  
